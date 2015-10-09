ad4e4942703d17c60c43e1026c56bb9ce3ad312b
wang2542 <wang2542@purdue.edu>
2015-10-09 14:05:45 -0400

    power

--- calc.py
+++ calc.py
@@ -33,6 +33,11 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+# Exp function
+# a
+# b
+def power(a, b):
+    return a ** b 
 
 # -------------------------------------------------------- #
 
@@ -67,8 +72,11 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+        elif (op == "**"):
+            print "exponent operator", power(a, b)
         else:
             print "Invalid operation..."
+        
 
     q = raw_input("Quit? [y/n] ")
     if (q == "y" or q == "Y"):
